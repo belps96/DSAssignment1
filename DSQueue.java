1efcab01474621f24d374d735f0f977111bb6376
belps96 <anth22b@hotmail.com>
2016-03-28 20:24:33 +1030

    Update DSQueue.java

--- DSQueue.java
+++ DSQueue.java
@@ -1,27 +1,30 @@
 
 public class DSQueue extends Queue {
-	
+
 	DSList List;
-	
+
 	public DSQueue(Queue input) {
-		List = new DSList((DSList)((List) input));
+		List = new DSList();
+		while (input.peek() != null){
+			List.add(input.poll());
+		}
 	}
-	
+
 	public DSQueue() {
 		List = new DSList();
 	}
-	
+
 	public boolean offer(Token t) {
 		if (t == null){
 			throw new NullPointerException();
 		}
 		return add(t);
 	}
-	
+
 	public Token poll() {
 		return List.remove(0);
 	}
-	
+
 	public Token peek() {
 
 		if (List.isEmpty() == true){
@@ -31,21 +34,21 @@ public class DSQueue extends Queue {
 			return List.get(0);
 		}
 	}
-	
+
 	@Override
 	public boolean add(Token obj) {
 		List.add(obj);
 		return true;
 	}
-	
+
 	public boolean isEmpty() {
 		return List.isEmpty();
 	}
-	
+
 	public int size() {
 		return List.size();
 	}
-	
+
 	@Override
 	public String toString() {
 		return List.toString();
